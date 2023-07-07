90fc76a8a721f0c96074a6c0c16773b0c2afd28b
tercioab <suaagenciainicial@gmail.com>
2023-07-07 00:34:56 -0300

    feat: add skilss and projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Introduction from "@/components/Sections/Introduction";
 import About from "@/components/Sections/About";
+import Skills from "@/components/Sections/Skills"
+import Projects from "@/components/Sections/Projects";
 export default function Home() {
   return (
     <main>
       <Introduction />
+      <Skills />
       <About />
-      
+      <Projects />
     </main>
   )
 }
