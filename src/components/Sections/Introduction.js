615f48c0561032eb5045f3523b0e6ef90100f3be
tercioab <suaagenciainicial@gmail.com>
2023-07-07 00:36:19 -0300

    feat: change bg and text

--- src/components/Sections/Introduction.js
+++ src/components/Sections/Introduction.js
@@ -1,16 +1,17 @@
 import Header from "../layout/Header";
 export default function Introduction() {
 	return (
-<section class="bg-no-repeat bg-cover bg-[url('https://images.unsplash.com/photo-1678581231067-644dddeca6dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80')] bg-gray-600 bg-blend-multiply">
-    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h2 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-            Você Me Encontrou!
-        </h2>
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-            Olá! Meu nome é Walthercio, sou um desenvolvedor web full-stack. Com experiência em mais de 30 projetos!
-        </p>
-    </div>
-</section>
-
+		<section class=" bg-no-repeat bg-cover bg-[url('https://images.unsplash.com/photo-1678581231067-644dddeca6dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80')] bg-gray-600 bg-blend-multiply">
+			<div class='px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56'>
+				<h2 class='mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl'>
+					Você Me Encontrou!
+				</h2>
+				<p class='mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48'>
+					Olá! Meu nome é Walthercio, Sou um desenvolvedor Web com ampla experiência em
+					projetos variados. Atuo como freelancer e já participei de mais de 30 projetos,
+					no desenvolvimento do front-end, back-end e projetos completos de ponta a ponta.
+				</p>
+			</div>
+		</section>
 	);
 }
