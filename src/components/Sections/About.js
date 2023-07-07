0db0ee71ad4f3eec462558b71b98e3daee409d42
tercioab <suaagenciainicial@gmail.com>
2023-07-07 00:32:34 -0300

    style

--- src/components/Sections/About.js
+++ src/components/Sections/About.js
@@ -2,14 +2,15 @@ import Steps from "./Steps";
 import ProjectResults from "./ProjectResults";
 export default function About() {
 	return (
-		<div className='flex justify-center bg-gray-100 border-t-8 border-gray-900'>
-  <div className='w-full max-w-screen-lg grid grid-cols-1 lg:grid-cols-2 gap-8 items-center'>
+		<div className='flex justify-center bg-gray-100 border-t-8 border-gray-100 mt-16'>
+  <div className='w-full max-w-screen-lg grid grid-cols-1 lg:grid-cols-2 gap-8 items-start'>
     <div className='relative'>
-      <div className='p-6 mx-auto'>
+      <div className='p-6 mx-auto '>
         <ProjectResults />
       </div>
     </div>
-    <div className='p-6 mx-auto'>
+        <div className='p-6 mx-auto'>
+          
       <Steps />
     </div>
   </div>
